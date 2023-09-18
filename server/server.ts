78b3eece13410c82125c7b84b054b9e99f4e430c
NattyNgrn <natnrgn@gmail.com>
2023-09-18 15:40:04 -0400

    set up pt 2

--- server/server.ts
+++ server/server.ts
@@ -5,14 +5,15 @@ import cors from "cors";
 
 
 Bun.serve({
-    port: 8080, // defaults to $BUN_PORT, $PORT, $NODE_PORT otherwise 3000
-    hostname: "natngrn.com", // defaults to "0.0.0.0"
+    port: 8080,
+    hostname: "natngrn.com",
+    development: true,
     fetch(req) {
         const url = new URL(req.url);
         if(url.pathname === "/") return new Response("homepage!");
         if(url.pathname === "/delete") return new Response("deleting");
         if(url.pathname === "/update") return new Response("updating");
         if(url.pathname === "/adding") return new Response("adding");
-        return new Response("404!");
+        throw new Error("woops!");
     },
 });
