a5d6b730c128ebc18066b5bcb089d0c4b6517f68
NattyNgrn <natnrgn@gmail.com>
2023-09-21 16:47:48 -0400

    fixing indenting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,56 +9,56 @@ import Animaltable from "./components/animaltable";
 //second is for sightings 
 
 function App() {
-  const [isShown, setIsShown] = useState(false);
-  const handleClick = event => { setIsShown2(false); setIsShown(true); }
-  const [data, setData] = useState([]);
+    const [pageToShow, setPageToShow] = useState("home");
 
-  const [isShown2, setIsShown2] = useState(false);
-  const handleClick2 = event => { setIsShown(false); setIsShown2(true); }
-  return (
+    switch (pageToShow) {
+        case "home" : return (<Home setPageToShow={setPageToShow}></Home>);
+        case "sightings": return (<Sightings setPageToShow={setPageToShow}></Sightings>);
+        case "individual": return (<Ind setPageToShow={setPageToShow}></Ind>);
+    }
+};
 
-      <div style={{
+function Home({setPageToShow}) {
+    
+    const handleClick = event => setPageToShow("sightings");
+    const handleClick2 = event => setPageToShow("individual");
+    const [data, setData] = useState([]);
+    
+    return (
+
+    <div style={{
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
         flexDirection: 'column'
-      }}>
+    }}>
+
         <div style={{
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'space-around',
             height: '100%',
         }}>
-        <div className="mt-6 flex items-center justify-end gap-x-6">
-        {!isShown ?
-          <button onClick={handleClick} style={{margin: '20px'}} className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-            Add Sighting
-          </button>
-          : <div></div>
-        } 
-        {/*if isshown is true then it shows the addevent component also passing refresh in so it refreshes the page when event is added */}
-        {isShown ? <Sightings setIsShown={setIsShown} ></Sightings> : <div></div>}
-        </div>
-        <div className="mt-6 flex items-center justify-end gap-x-6">
 
-        <div>
-        { !isShown2 ?
-          
-          <button onClick={handleClick2} style={{margin: '20px'}} className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-            Add a New Animal
-          </button>
-          : <div></div>
-        } 
-        {/*if isshown is true then it shows the addevent component also passing refresh in so it refreshes the page when event is added */}
-        {isShown2 ? <Ind setIsShown={setIsShown2} ></Ind> : <div></div>}
+            <div className="mt-6 flex items-center justify-end gap-x-6">
+                <button onClick={handleClick} style={{margin: '20px'}} className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    Add Sighting
+                </button>
+            </div>
+
+            <div className="mt-6 flex items-center justify-end gap-x-6">         
+                <button onClick={handleClick2} style={{margin: '20px'}} className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    Add a New Animal
+                </button>
+            </div>
+
         </div>
 
-
-          </div>
-          </div>
         <Animaltable data={data}/>
-        </div>
-  )
+
+    </div>
+
+    );
 }
 
 export default App;
